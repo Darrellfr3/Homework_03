b8d557684641dcea6bc842814a6889bcdd63614e
darrellfr3 <darrellfr3@gmail.com>
2020-10-31 23:52:37 -0500

    Added comments to program

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-//
+//Event listener that initiates program
 generateBtn.addEventListener("click",function generatePassword(){
-  //
+  //Prompts user for password length and validates
   var passLength = prompt("Please enter the character length you wish your password to be: ");
   while(passLength.length >= 8 && passLength.length <= 128){
     alert("Length must be either a minimum of 8 characters or maximum of 128!");
     var passLength = prompt("Please enter the character length you wish your password to be: ");
   }
 
-  //
+  //Prompts user for password criteria and validates
   var characterValid = 0;
 
   var upperCaseCriteria = confirm("Press 'ok' if you want uppercase characters in your password!");
@@ -49,7 +49,6 @@ generateBtn.addEventListener("click",function generatePassword(){
     var specialCharPass = "";
   }
 
-  //
   while(characterValid === 0){
     alert("Atleast one character type must be selected!");
 
@@ -90,17 +89,18 @@ generateBtn.addEventListener("click",function generatePassword(){
   }
   }
 
-  //
+  //Declares password and object used to pass characters to password
   var passGen = upperCasePass + lowerCasePass + numericPass + specialCharPass;
   var password = '';
 
-  //
+  //Generates password
   for(var counter = 0; counter < passLength; counter++){
     var char = Math.floor(Math.random() * passGen.length + 1);
 
     password += passGen[char];
   }
 
+  //Passes password to text in html
   var passwordText = document.querySelector("#password");
 
   passwordText.textContent = password;
